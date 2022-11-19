dc73227982cdd1019941804b65b9c69d84488437
drashtant09 <gangwar.drashtant@gmail.com>
2022-11-20 00:06:46 +0530

    4th commit

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+    {
+        name: "DANCE KA BHOOT",
+        artist: "...Arijit Singh...",
+        img: "image-4",
+        src: "image-4",
+    },
+    {
+        name: "RANG JO LAGYO",
+        artist: "...Atif Aslam, Shreya Ghoshal...",
+        img: "image-8",
+        src: "image-8",
+    },
+    {
+        name: "TU AAKE DEKHLE",
+        artist: "...King...",
+        img: "image-3",
+        src: "image-3",
+    },
+    {
+        name: "BESABRIYAAN",
+        artist: "...Armaan Malik...",
+        img: "image-7",
+        src: "image-7",
+    },
+    {
+        name: "SHUBHAARAMBH",
+        artist: "...Shruti Pathak...",
+        img: "image-5",
+        src: "image-5",
+    },
+    {
+        name: "KESARIYA",
+        artist: "...Arijit Singh...",
+        img: "image-1",
+        src: "image-1",
+    },
+    {
+        name: "LEMONADE",
+        artist: "...Diljit Dosanjh...",
+        img: "image-2",
+        src: "image-2",
+    },
+    
+]
