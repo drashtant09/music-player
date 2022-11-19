744e33b140074df2b7fcc5709910b90b1ab825bb
drashtant09 <gangwar.drashtant@gmail.com>
2022-11-20 00:22:24 +0530

    4th commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// lets select all required tags or elements
+
+const wrapper = document.querySelector(".wrapper"),
+musicImg = document.querySelector(".img-area img"),
+musicName = document.querySelector(".song-details .name"),
+musicArtist = document.querySelector(".song-details .artist");
+
+let musicIndex = 2;
