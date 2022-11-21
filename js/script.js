9e80a49ccd73e3edf04a009b6a76b00c61547ebd
drashtant09 <gangwar.drashtant@gmail.com>
2022-11-21 23:04:22 +0530

    4th commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 // lets select all required tags or elements
 
 const wrapper = document.querySelector(".wrapper"),
-musicImg = wrapper.querySelector(".img-area img"),
+musicImg = document.querySelector(".img-area img"),
 musicName = wrapper.querySelector(".song-details .name"),
 musicArtist = wrapper.querySelector(".song-details .artist"),
 mainAudio = wrapper.querySelector("#main-audio");
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next"),
+progressBar = wrapper.querySelector(".progress-bar");
 
 let musicIndex = 2;
 
@@ -16,5 +20,71 @@ function loadMusic(indexNumb){
     musicName.innerText = allMusic[indexNumb - 1].name;
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
     musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpeg`;
-    mainAudio.src = `songs/${allMusic[indexNumb - -1].src}.mp3`;
+    mainAudio.src = `songs/${allMusic[indexNumb -1].src}.mp3`;
+
 }
+
+//play music function 
+function playMusic() {
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play();
+}
+
+//pause music function 
+function pauseMusic() {
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause();
+}
+
+function nextMusic(){
+    musicIndex++;
+    musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+}
+
+function prevMusic(){
+    musicIndex--;
+    musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+}
+
+//play or music button event
+playPauseBtn.addEventListener("click", ()=>{
+    const isMusicPaused = wrapper.classList.contains("paused");
+    isMusicPaused ? pauseMusic() : playMusic();
+});
+
+
+nextBtn.addEventListener("click", ()=>{
+    nextMusic();
+});
+
+prevBtn.addEventListener("click", ()=>{
+    prevMusic();
+});
+
+// update progress bar width according to music current time
+
+mainAudio.addEventListener("timeupdate", (e)=>{
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+    
+    mainAudio.addEventListener("loadeddata", ()=>{
+        let musicCurrentTime = wrapper.querySelector(".current"),
+        musicDuration = wrapper.querySelector(".duration");
+        
+        let audioDuration = minAudio.duration;
+        let totalMin = math.floor(audioDuration / 60);
+        let totalSec = math.floor(audioDuration % 60);
+        if(totalSec < 10){
+            totalSec = `0${totalSec}`;
+        }
+        musicDuration.innerText = `${totalMin}:${totalSec}`;
+    });
+});
